84c04e304c9d9847973255bda65de7f8c1f0dbfa
BradFernando <bfcorro@espe.edu.ec>
2024-04-18 20:43:36 -0500

    Actualizacion de Inicio 4

--- pages/api/sendToTelegram.js
+++ pages/api/sendToTelegram.js
@@ -3,22 +3,19 @@ import fetch from 'node-fetch';
 
 export default async function handler(req, res) {
   if (req.method === 'POST') {
-    // Aquí es donde enviarías los datos a Telegram
+    // Aquí es donde enviarías los datos a tu servidor Python
     const data = req.body;
 
-    const response = await fetch('https://api.telegram.org/bot6556491083:AAFKdXsUErUqMhHzsMNZhzkmN5OXWI7us0Y/sendMessage', {
+    const response = await fetch('http://localhost:5000/api/receiveData', { // reemplaza esto con la URL de tu servidor Python
       method: 'POST',
       headers: {
         'Content-Type': 'application/json'
       },
-      body: JSON.stringify({
-        chat_id: '2084365198', // reemplaza esto con el ID de tu chat
-        text: JSON.stringify(data, null, 2)
-      })
+      body: JSON.stringify(data)
     });
 
     if (!response.ok) {
-      console.error('Failed to send data to Telegram');
+      console.error('Failed to send data to Python server');
       res.status(500).json({ status: 'error' });
       return;
     }
